dfee9cfcb56ad0099df78cf2552f892dfa50d367
badassmutherfucker <johan.toverland@gmail.com>
2017-10-09 11:34:11 +0200

    adding some files

---
+++ Johanna/edge_detection/edge_detection/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace edge_detection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("D:\\hand.jpg");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ColorMatrix mymatrix = new ColorMatrix(new float[][]
+              {
+                            new float[]{-2, 0, 0, 0, 0},
+                            new float[]{0, -1, 0, 0, 0},
+                            new float[]{0, 0, -1, 0, 0},
+                            new float[]{0, 0, 0, 1, 0},
+                            new float[]{1, 1, 1, 1, 1}
+              });
+
+            ColorMatrix amatrix = new ColorMatrix(new float[][]
+                {
+                            new float[]{ -1, -1, -1, -1, -1, },
+                            new float[]{ -1,  2,  2,  2, -1, },
+                            new float[]{ -1,  2,  8,  2,  1, },
+                            new float[]{ -1,  2,  2,  2, -1, },
+                            new float[]{ -1, -1, -1, -1, -1, }
+
+                });
+
+            Bitmap original = new Bitmap("D:\\hand.jpg");
+            Bitmap hej = Prewitt.ExtBitmap.ApplyColorMatrix(original, mymatrix);
+            //Bitmap final = Prewitt.ExtBitmap.PrewittFilter(hej);
+            Bitmap final = Prewitt.ExtBitmap.ApplyColorMatrix(hej, amatrix);
+
+            pictureBox1.Image = final;
+        }
+    }
+}
+
+namespace Prewitt
+{ 
+    public static class ExtBitmap
+    {
+        private static Bitmap ConvolutionFilter(Bitmap sourceBitmap,
+                                             double[,] filterMatrix,
+                                                  double factor = 1,
+                                                       int bias = 0,
+                                             bool grayscale = false)
+        {
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                     sourceBitmap.Width, sourceBitmap.Height),
+                                                       ImageLockMode.ReadOnly,
+                                                 PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            if (grayscale == true)
+            {
+                float rgb = 0;
+
+                for (int k = 0; k < pixelBuffer.Length; k += 4)
+                {
+                    rgb = pixelBuffer[k] * 0.11f;
+                    rgb += pixelBuffer[k + 1] * 0.59f;
+                    rgb += pixelBuffer[k + 2] * 0.3f;
+
+
+                    pixelBuffer[k] = (byte)rgb;
+                    pixelBuffer[k + 1] = pixelBuffer[k];
+                    pixelBuffer[k + 2] = pixelBuffer[k];
+                    pixelBuffer[k + 3] = 255;
+                }
+            }
+
+            double blue = 0.0;
+            double green = 0.0;
+            double red = 0.0;
+
+            int filterWidth = filterMatrix.GetLength(1);
+            int filterHeight = filterMatrix.GetLength(0);
+
+            int filterOffset = (filterWidth - 1) / 2;
+            int calcOffset = 0;
+
+            int byteOffset = 0;
+
+            for (int offsetY = filterOffset; offsetY <
+                sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX <
+                    sourceBitmap.Width - filterOffset; offsetX++)
+                {
+                    blue = 0;
+                    green = 0;
+                    red = 0;
+
+                    byteOffset = offsetY *
+                                 sourceData.Stride +
+                                 offsetX * 4;
+
+                    for (int filterY = -filterOffset;
+                        filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset;
+                            filterX <= filterOffset; filterX++)
+                        {
+
+                            calcOffset = byteOffset +
+                                         (filterX * 4) +
+                                         (filterY * sourceData.Stride);
+
+                            blue += (double)(pixelBuffer[calcOffset]) *
+                                    filterMatrix[filterY + filterOffset,
+                                                        filterX + filterOffset];
+
+                            green += (double)(pixelBuffer[calcOffset + 1]) *
+                                     filterMatrix[filterY + filterOffset,
+                                                        filterX + filterOffset];
+
+                            red += (double)(pixelBuffer[calcOffset + 2]) *
+                                   filterMatrix[filterY + filterOffset,
+                                                      filterX + filterOffset];
+                        }
+                    }
+
+                    blue = factor * blue + bias;
+                    green = factor * green + bias;
+                    red = factor * red + bias;
+
+                    if (blue > 255)
+                    { blue = 255; }
+                    else if (blue < 0)
+                    { blue = 0; }
+
+                    if (green > 255)
+                    { green = 255; }
+                    else if (green < 0)
+                    { green = 0; }
+
+                    if (red > 255)
+                    { red = 255; }
+                    else if (red < 0)
+                    { red = 0; }
+
+                    resultBuffer[byteOffset] = (byte)(blue);
+                    resultBuffer[byteOffset + 1] = (byte)(green);
+                    resultBuffer[byteOffset + 2] = (byte)(red);
+                    resultBuffer[byteOffset + 3] = 255;
+                }
+            }
+
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+                                     resultBitmap.Width, resultBitmap.Height),
+                                                      ImageLockMode.WriteOnly,
+                                                 PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+
+        public static Bitmap PrewittFilter(Bitmap sourceBitmap, bool grayscale = true)
+        {
+            double[,] Prewitt3x3Horizontal = new double[,]
+                { { -1,  0,  1, },
+                { -1,  0,  1, },
+                { -1,  0,  1, }, };
+
+            double[,] Prewitt3x3Vertical = new double[,]
+                { {  1,  1,  1, },
+                {  0,  0,  0, },
+                { -1, -1, -1, }, };
+
+
+            Bitmap resultBitmap =
+                   ExtBitmap.ConvolutionFilter(sourceBitmap,
+                                Prewitt3x3Horizontal,
+                                          1.0, 0, grayscale = false);
+
+            return resultBitmap;
+        }
+
+        public static Bitmap GetArgbCopy(Image sourceImage)
+        {
+            Bitmap bmpNew = new Bitmap(sourceImage.Width, sourceImage.Height, PixelFormat.Format32bppArgb);
+
+
+            using (Graphics graphics = Graphics.FromImage(bmpNew))
+            {
+                graphics.DrawImage(sourceImage, new Rectangle(0, 0, bmpNew.Width, bmpNew.Height), new Rectangle(0, 0, bmpNew.Width, bmpNew.Height), GraphicsUnit.Pixel);
+                graphics.Flush();
+            }
+
+            return bmpNew;
+        }
+
+        public static Bitmap ApplyColorMatrix(Image sourceImage, ColorMatrix colorMatrix)
+        {
+            Bitmap bmp32BppSource = GetArgbCopy(sourceImage);
+            Bitmap bmp32BppDest = new Bitmap(bmp32BppSource.Width, bmp32BppSource.Height, PixelFormat.Format32bppArgb);
+
+
+            using (Graphics graphics = Graphics.FromImage(bmp32BppDest))
+            {
+                ImageAttributes bmpAttributes = new ImageAttributes();
+                bmpAttributes.SetColorMatrix(colorMatrix);
+
+                graphics.DrawImage(bmp32BppSource, new Rectangle(0, 0, bmp32BppSource.Width, bmp32BppSource.Height),
+                                    0, 0, bmp32BppSource.Width, bmp32BppSource.Height, GraphicsUnit.Pixel, bmpAttributes);
+            }
+            bmp32BppSource.Dispose();
+
+            return bmp32BppDest;
+        }
+    }
+
+}
---
+++ Johanna/test/test/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new AForge.Controls.PictureBox();
+            this.pictureBox2 = new AForge.Controls.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = null;
+            this.pictureBox1.Location = new System.Drawing.Point(86, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 108);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = null;
+            this.pictureBox2.Location = new System.Drawing.Point(307, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(115, 108);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 244);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.PictureBox pictureBox1;
+        private AForge.Controls.PictureBox pictureBox2;
+    }
+}
+
