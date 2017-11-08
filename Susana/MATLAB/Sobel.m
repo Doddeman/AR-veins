ba8c7dbf3e98a7e3edbf66cce327f01a3aafdd0c
johannaalfredsson <johanna.alfredsson@hotmail.com>
2017-11-08 14:46:07 +0100

    meer filteeeer

--- Susana/MATLAB/Sobel.m
+++ Susana/MATLAB/Sobel.m
@@ -18,7 +18,7 @@ Image = uint8(Iglb);
 
 figure(1)
 subplot(2,2,1)
-imshow('jeppacut.png')
+imshow('jeppa.png')
 subplot(2,2,2)
 imshow(Image)
 
@@ -44,7 +44,6 @@ imshow(median)
 
 final = padzeros(Image);
 
-
 [Gx, Gy] = imgradientxy(smoothie, 'sobel');
 [Fx, Fy] = imgradientxy(smoothie, 'central');
 [Px, Py] = imgradientxy(smoothie, 'prewitt');
@@ -58,9 +57,6 @@ subplot(1, 2, 2)
 imshow(Fy)
 title('central')
 
-figure(12)
-nohole = imfill(Fy, 'holes');
-imshow(nohole)
 
 figure(13)
 mesh(Image)
@@ -73,17 +69,38 @@ binary = imbinarize(Fy, 1);
 figure(14)
 imshow(binary)
 
+medianbinary = medfilt2(binary, [4,4]);
+
 figure(15)
-subplot(1,2,1)
+subplot(2,2,1)
 imshow(binary, [1 1 1; 1 0 0])
-subplot(1,2,2)
+subplot(2,2,2)
 imshow(binary, [0 0 0; 1 0 0])
-
-medianbinary = medfilt2(binary, [5,5]);
-
-figure(16)
+subplot(2,2,3)
 imshow(medianbinary)
+subplot(2,2,4)
+imshow(medianbinary, [0 0 0; 1 0 0])
+
+figure(89)
+pic = imgaussfilt(double(binary), 6);
+imshow(pic)
 
 
 
+%remove bad light
+background = imopen(pic,strel('disk',100));
+
+figure(188)
+surf(double(background(1:8:end,1:8:end))),zlim([0 255]);
+set(gca,'ydir','reverse');
+
+figure(27)
+I2 = pic - background;
+imshow(I2)
+
+figure(80)
+level = graythresh(pic);
+hej = imbinarize(I2, 0.2);
+
+imshow(hej)
 
