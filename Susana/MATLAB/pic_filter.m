95650b94b22a7b575380a1d24b8b836d872bb8da
Susana <susanabardolet@gmail.com>
2017-11-20 17:01:31 +0100

    matlab

--- Susana/MATLAB/pic_filter.m
+++ Susana/MATLAB/pic_filter.m
@@ -1,7 +1,7 @@
 function picture = pic_filter(pic)
-image = imread(pic); %3D
+image = imread('jeppacut.png'); %3D
 image2 = rgb2gray(image); % 2D
-A = adapthisteq(image2,'ClipLimit',0.015); % contrast
+A = adapthisteq(image2,'ClipLimit',0.015) % contrast
 level = graythresh(A); % threshold value calculated to binarize the image
 BA = imbinarize(A, 0.51);% Binarize the image 
 median = medfilt2(BA); % Median filter 
@@ -10,7 +10,7 @@ fill = imfill(median, 'holes');
 %Remove blobs that are smaller than X pixels across
 se = strel('diamond', 10);
 pic_end = imopen(fill, se);
-figure(1);
+figure(2);
 subplot(2,3,1)
 imshow(image);
 subplot(2,3,2)
@@ -23,7 +23,6 @@ subplot(2,3,5)
 imshow(fill);
 subplot(2,3,6)
 imshow(pic_end, [0 0 0; 1 0 0]);
-
 end
 
 
